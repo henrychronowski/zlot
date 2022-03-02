32f4117e8bf860d3805c681083437a6caaaf104a
Ethan Heil <eheil20@gmail.com>
2022-03-02 15:16:30 -0500

    Adding vertex and pixel shader declaration and binding

---
+++ VolumetricRaymarcher/Source/VolumetricRaymarcher/RayMarcher.cpp
@@ -0,0 +1,5 @@
+#include "RayMarcher.h"
+
+IMPLEMENT_SHADER_TYPE(, FRayMarcherVS, TEXT("RayMarchLighting"), TEXT("MainVS"), SF_Vertex);
+
+IMPLEMENT_SHADER_TYPE(, FRayMarcherVS, TEXT("RayMarchLighting"), TEXT("MainPS"), SF_Pixel);
